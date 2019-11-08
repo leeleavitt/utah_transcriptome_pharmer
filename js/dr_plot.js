ccfdd8634f151a31b8db478e3662861231e52030
LeeHome <lee.leavitt.u@gmail.com>
2019-11-08 10:00:13 -0700

    Currently my messy global variable SVD and matrix math on script.js

--- js/dr_plot.js
+++ js/dr_plot.js
@@ -8,10 +8,16 @@ class drPlot{
         this.dataSet = dataSet
         console.log(dataSet)
 
-        dataSetMat = new ML.Matrix(dataSet)
-    
-        var data = [[40,50,60],[50,70,60],[80,70,90],[50,60,80]];
-        var vectors = new ML.PCA(data);
+        var dataSetArray = dataSet.map(obj => Object.values(obj))
+        dataSetArray = dataSetArray.map(d =>{
+            let byeVal = d.shift()
+            return d
+        })
+        console.log(dataSetArray)
+        
+        //Start the SVD 
+        
+        var vectors = new ML.SVD(dataSetArray);
         console.log(vectors)
         
     }
--- rtesting.r
+++ rtesting.r
@@ -1,37 +1,16 @@
 data <- read.csv('./data/GSE131230_counts_official.csv')
-row.names(data) <- data[,1]
-data<-data[-1]
 
-dataPca<-prcomp(data[-1])
-head(dataPca)
+row.names(data)<-data[,1]
+data <- data[-1]
+dataSvd <- svd(data)
 
+pc1 = dataSvd$d[1]*dataSvd$u[1,]
+pc2 = dataSvd$d[2]*dataSvd$u[2,]
 
+plot(pc1,pc2)
 
-# NOT RUN {
-C <- chol(S <- toeplitz(.9 ^ (0:31))) # Cov.matrix and its root
-all.equal(S, crossprod(C))
-set.seed(17)
-X <- matrix(rnorm(32000), 1000, 32)
-Z <- X %*% C  ## ==>  cov(Z) ~=  C'C = S
-all.equal(cov(Z), S, tol = 0.08)
-pZ <- prcomp(Z, tol = 0.1)
-summary(pZ) # only ~14 PCs (out of 32)
-## or choose only 3 PCs more directly:
-pz3 <- prcomp(Z, rank. = 3)
-summary(pz3) # same numbers as the first 3 above
-stopifnot(ncol(pZ$rotation) == 14, ncol(pz3$rotation) == 3,
-          all.equal(pz3$sdev, pZ$sdev, tol = 1e-15)) # exactly equal typically
+text(pc1,pc2, colnames(data))
 
-# }
-# NOT RUN {
-## signs are random
-require(graphics)
-## the variances of the variables in the
-## USArrests data vary by orders of magnitude, so scaling is appropriate
-prcomp(USArrests)  # inappropriate
-prcomp(USArrests, scale = TRUE)
-prcomp(~ Murder + Assault + Rape, data = USArrests, scale = TRUE)
-plot(prcomp(USArrests))
-summary(prcomp(USArrests, scale = TRUE))
-biplot(prcomp(USArrests, scale = TRUE))
-# }
+pcaData <- prcomp(data)
+
+biplot(pcaData)
