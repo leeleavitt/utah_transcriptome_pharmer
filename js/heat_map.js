ba97fc309cd826e09eccf4306f9695e0a0a858bb
tjtiner <travisjtiner@gmail.com>
2019-11-08 00:37:23 -0700

    added row and column sorting to heatmap

--- js/heat_map.js
+++ js/heat_map.js
@@ -6,21 +6,29 @@ class Heatmap{
 
     this.height = 900 - this.margin.top - this.margin.bottom;
 
-    this.heatmapData = data.slice(0,100);
+    this.heatmapData = data.slice(0,50);
+
+		console.log(this.heatmapData);
+
+		this.sorted = false;
 
     this.cells = Object.keys(this.heatmapData[0]).splice(1);
+		this.cells.sort();
 
     this.genes = this.heatmapData.map(d => d[""]);
   }
 
   createHeatmap() {
 
+		this.genes = this.heatmapData.map(d => d[""]);
+		console.log(this.genes);
+
     this.cellsGroups = [...new Set(this.cells.map(d => d.slice(0,-2)))];
 
     let cellsColorScale = d3.scaleOrdinal(d3.schemeSet2)
         .domain(this.cellsGroups);
 
-    let stretched_data = [];
+    this.stretched_data = [];
 
     for (let i = 0; i < this.heatmapData.length; i++){
       let vals = []
@@ -32,7 +40,7 @@ class Heatmap{
       var min = Math.min(...vals);
 
       for (let j = 0; j < this.cells.length; j++){
-        stretched_data.push({"gene": this.heatmapData[i][""], "cell": this.cells[j],
+        this.stretched_data.push({"gene": this.heatmapData[i][""], "cell": this.cells[j],
         "value": ((parseFloat(this.heatmapData[i][this.cells[j]]) - min)/(Math.max(1,(max - min)))), "actualValue": parseFloat(this.heatmapData[i][this.cells[j]])})
       }
     }
@@ -53,7 +61,7 @@ class Heatmap{
       .padding(0.01);
 
   svg.append("g")
-    .attr("transform", "translate(0,0)")
+		.attr("id","xAxis")
     .call(d3.axisTop(x))
     .selectAll("text")
     .attr("transform","translate(0,-20) rotate(-20)")
@@ -68,9 +76,6 @@ var yAxis = svg.append("g")
   .attr("id","yAxis")
   .call(d3.axisLeft(y));
 
-yAxis.selectAll('text')
-  .attr("class","t1");
-
 d3.select('#yAxis')
   .selectAll('g')
   .data(this.cells)
@@ -80,8 +85,10 @@ d3.select('#yAxis')
   .attr("height", y.bandwidth())
   .attr("width", 150)
   .style("fill", d => cellsColorScale(d.slice(0,-2)))
-  .attr("opacity",0.5);
+  .attr("opacity",0.2);
 
+d3.select('#yAxis').selectAll('rect').on('click',d => this.sortCols(d));
+d3.select('#xAxis').selectAll('text').on('click',d => this.sortRows(d));
 
 // Build color scale
 var myColor = d3.scaleLinear()
@@ -97,8 +104,12 @@ var myColor = d3.scaleLinear()
 
 
 //Read the data
-  svg.selectAll()
-      .data(stretched_data)
+  let rectGroup = svg
+			.append('g')
+			.attr('id','rectGroup');
+
+	rectGroup.selectAll()
+      .data(this.stretched_data)
       .enter()
       .append("rect")
       .attr("x", d => x(d.gene))
@@ -111,14 +122,82 @@ var myColor = d3.scaleLinear()
                               .style("left", (d3.event.pageX + 10) + "px")
                               .style("top", (d3.event.pageY - 35) + "px"))
       .on("mouseout",d => div.style("opacity",0));
-
+			console.log(this.heatmapData);
   }
 
   updateHeatmap() {
-    return
+		this.genes = this.heatmapData.map(d => d[""]);
 
+		var x = d3.scaleBand()
+			.range([ 0, this.width ])
+			.domain(this.genes)
+			.padding(0.01);
+
+		var y = d3.scaleBand()
+		  .range([0, this.height])
+		  .domain(this.cells)
+		  .padding(0.01);
+
+		d3.select('#xAxis')
+		.transition()
+		.duration(1500)
+		.call(d3.axisTop(x));
+
+		d3.select('#yAxis')
+		.transition()
+		.duration(1500)
+		.call(d3.axisLeft(y));
+
+		d3.select('#rectGroup')
+		.selectAll('rect')
+		.join(this.stretched_data)
+		.transition()
+		.duration(1500)
+		.attr("x", d => x(d.gene))
+		.attr("y", d => y(d.cell))
+		.attr("width", x.bandwidth() )
+		.attr("height", y.bandwidth() )
   }
 
+sortCols(cell){
+	if (this.sorted){
+		this.heatmapData = this.heatmapData.sort((a, b) =>
+			parseFloat(a[cell]) < parseFloat(b[cell]) ? -1 : 1);
+	}else {
+		this.heatmapData = this.heatmapData.sort((a, b) =>
+		parseFloat(a[cell]) > parseFloat(b[cell]) ? -1 : 1);
+	}
+
+
+	this.sorted = !this.sorted;
+
+	this.updateHeatmap();
+}
+
+sortRows(row) {
+	let gene = JSON.parse(JSON.stringify(this.heatmapData[this.genes.indexOf(row)]));
+	delete gene[""];
+	var sortable = [];
+	for (var cell in gene) {
+	    sortable.push([cell, parseFloat(gene[cell])]);
+	}
+
+	if (this.sorted){
+		sortable = sortable.sort((a, b) =>
+		a[1] < b[1] ? -1 : 1);
+	}else {
+		sortable = sortable.sort((a, b) =>
+		a[1] > b[1]  ? -1 : 1);
+	}
+
+	this.cells = [];
+	for (let i = 0; i < sortable.length; i++){
+		this.cells.push(sortable[i][0]);
+	}
+
+	this.sorted = !this.sorted;
+	this.updateHeatmap();
+}
 
 tooltipRender(data) {
     let text = "<h2>" + "Gene: " + data['gene'] + "<br>" + "Cell: " + data['cell'] +
