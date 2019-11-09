4704379db9d6e8b41add0c8861c468c608e19e0b
LeeHome <lee.leavitt.u@gmail.com>
2019-11-08 19:52:27 -0700

    Continued SVD exploration

--- rtesting.r
+++ rtesting.r
@@ -2,12 +2,16 @@ data <- read.csv('./data/GSE131230_counts_official.csv')
 
 row.names(data)<-data[,1]
 data <- data[-1]
-dataSvd <- svd(data)
+dataSvd <- svd(t(data))
 
 dataPCA <- prcomp(data)
 
-pc1 = dataSvd$d[1]*dataSvd$u[1,]
-pc2 = dataSvd$d[2]*dataSvd$u[2,]
+pc1 = dataSvd$d[1]*dataSvd$u[,1]
+pc2 = dataSvd$d[2]*dataSvd$u[,2]
+
+dc1 = dataSvd$d[1] * dataSvd$v[,1]
+dc2 = dataSvd$d[2] * dataSvd$v[,2]
+
 
 plot(pc1,pc2)
 
