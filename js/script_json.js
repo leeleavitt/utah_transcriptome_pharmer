606520c4fbf3573ed5e6bf0873e5d7dae0cf625b
studydude <studydude@protonmail.com>
2019-11-13 17:41:07 -0700

    change json object to array

--- js/script_json.js
+++ js/script_json.js
@@ -1,4 +1,8 @@
 d3.json('data_preprocessing/final_data.json').then(data => {
+	/* convert json object to array */
+	let dataArray = Object.values(data);
+		console.log(dataArray);
 
-		console.log(data);
+		dataArray.slice(0,20);
+	
 });
