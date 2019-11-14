f94c88ec7d41bcc7f9058d09ac6f5ed5585721f5
LeeHome <lee.leavitt.u@gmail.com>
2019-11-13 22:29:06 -0700

    Got the new data set to work with my Functions. Much easier to work with

--- js/script_json.js
+++ js/script_json.js
@@ -1,5 +1,44 @@
-d3.json('data_preprocessing/final_data.json').then(datum => {
+d3.json('data_preprocessing/final_data.json').then(data => {
+	/* convert json object to array */
+	dataTotal = Object.values(data);
 
-		datums = datum
->>>>>>> 08bd5c5132af453119d181d88c1fbea4681f97ae
+	//Start the data 
+	terms = ['ion channel', 'G-protein']
+
+	genes = []
+
+	for(var i=0; i<terms.length; i++){
+		// Turn this into array
+		dataTotalArray = Object.values(dataTotal)
+		//Filter all genes
+		dataSubset = dataTotalArray.filter(d=>{
+			//filter if the array of go terms have a match for the term
+			let goTermsVal = d['GO.term.name'].filter(d=>d.match(terms[i]))
+			return goTermsVal.length > 0
+		})
+		genes = genes.concat(dataSubset)
+	}
+
+	//Remove duplicate genes.
+	genes = [...new Set(genes)]
+
+	//Some Gene values are null so remove them
+	genes = genes.filter(d=>d.cell_values!=null)
+
+	//Remove data that are completely 0's
+	genesNoZeroData = genes.filter(d=>{
+		let cellVals = Object.values(d.cell_values)
+		let cellValsTot = cellVals.reduce((a,b)=>a+b);
+		return cellValsTot >= 0
+	})
+
+	let drplot =  new drPlot(genesNoZeroData);
+	drplot.pcaCompute();
+	drplot.createPlot();
+	drplot.drawPlot();
+
+
+
+
+
 });
