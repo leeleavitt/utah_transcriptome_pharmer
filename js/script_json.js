eb1470a011c327370ce99f4a1af8269889198a01
leeWork <lee.leavitt.u@gmail.com>
2019-11-18 11:55:57 -0700

    not sure

--- js/script_json.js
+++ js/script_json.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-d3.json('data_preprocessing/final_data.json').then(datum => {
-
-		datums = datum
-=======
 d3.json('data_preprocessing/final_data.json').then(data => {
 	/* convert json object to array */
 	let dataArray = Object.values(data);
@@ -10,5 +5,4 @@ d3.json('data_preprocessing/final_data.json').then(data => {
 
 		dataArray.slice(0,20);
 	
->>>>>>> 606520c4fbf3573ed5e6bf0873e5d7dae0cf625b
 });
