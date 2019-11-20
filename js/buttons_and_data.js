595f5a5fa24b04b702394126ed81dd01e09c171b
leeleavitt <lee.leavitt.u@gmail.com>
2019-11-19 20:12:13 -0700

    Adding the data buttons

--- js/buttons_and_data.js
+++ js/buttons_and_data.js
@@ -6,30 +6,31 @@ class Setup{
     }
 
     initial(){
-
-      //Hierarchical Clustering Button
+      //////////////////////////////////////////////////////////
+      //Hierarchical Clustering Buttons
       let clustButtons = d3.select('#buttons')
         .append('div')
         .attr('id','clustButtons');
 
       clustButtons.append('button')
-      .attr('class','btn btn-primary clustButton')
-      .attr('id','hClustButton')
-      .attr('data-toggle','button')
-      .attr('aria-pressed','true')
-      .on('click',d => this.heatmap.hClustering())
-      .text('Hierarchical Clustering');
+        .attr('class','btn btn-primary clustButton')
+        .attr('id','hClustButton')
+        .attr('data-toggle','button')
+        .attr('aria-pressed','true')
+        .on('click',d => this.heatmap.hClustering())
+        .text('Hierarchical Clustering');
 
-
+      ////////////////////////////////////////////////////////////////////
+      //Cell Type Buttons
       //Grab the first cell values, this contains cell types names
-      cells = Object.getOwnPropertyNames(dataTotal[0].cell_values)
+      var cells = Object.getOwnPropertyNames(this.data[0].cell_values)
       //Remove the number identifier at the and and obtain a unique set
-      cellsUnique = [...new Set(cells.map(d=>d.slice(0,-2)))]
+      var cellsUnique = [...new Set(cells.map(d=>d.slice(0,-2)))]
 
       //Creat a list of cell types objects with logic of the buttons
-      cellsLogic = []
+      var cellsLogic = []
       cellsUnique.map((d,i)=>{
-        cellsUniqueLogic = {}
+        let cellsUniqueLogic = {}
         cellsUniqueLogic['cells']= d
         cellsUniqueLogic['logic'] = true
         cellsLogic[i] = cellsUniqueLogic
@@ -66,11 +67,60 @@ class Setup{
           that.heatmap.removeCell(d);
         })
         .text(d=>d)
+      /////////////////////////////////////////////////////
+      //Data Operation Buttons. We need
+      // Center
+      // Scale
+      // Row normalize
+      // Collumn normalize
+      // Whole Table Normalize
+      var dataButtonVals = ['Center','Scale','Row_Normalize', 'Collumn_Normalize', 'Whole_Table_Normalize']
+
+      //Use my logic object technique
+      var datasLogic = []
+      dataButtonVals.map((d,i)=>{
+        let buttonLogic = {}
+        buttonLogic['dataButton'] = d
+        buttonLogic['logic'] = false
+        dataLogic[i] = buttonLogic
+      })
+
+      //Make div for the data button
+      d3.select('#buttons')
+        .append('div')
+        .attr('id', 'dataButtons')
+
+      let dataButtonHolder = d3.select('#dataButtons')
+
+      dataButtonHolder
+        .append('h6')
+        .text('Data Transformations')
+
+      let dataButton = dataButtonHolder.selectAll('button')
+        .data(datasLogic)
+
+      let dataButtonEnter = dataButton.enter()
+
+      databutton = dataButtonEnter.merge(dataButton)
+        .append('button')
+        .attr('class','btn btn-secondary dataButton active')
+        .attr('id', d=>`${d}Button`)
+        .attr('data-toggle','button')
+        .on('click', function(d) {
+          that.buttonChecker(d);
+          that.heatmap.removeCell(d);
+        })
+        .text(d=>d.replace('_',' '))
+        .on('click', d=>{
+          print(d)
+        })
 
 
+      
 
 
     }
+
 
     //This function update the button logic as well as the pca plot for now
   	buttonChecker(cells){
@@ -103,5 +153,17 @@ class Setup{
   		this.drPlot.pcaCompute(cellsLogic);
   		//drplot.createPlot();
   		this.drPlot.drawPlot();
-  		}
+      }
+    
+    /////////////////////////////////////////////////////////////////////////
+    //Data Operations
+    ////////////////////////////////////////////////////////////////////////
+    
+
+
+
+
+
+
+    
     }
